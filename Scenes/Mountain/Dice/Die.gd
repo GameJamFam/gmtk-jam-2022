61fa26b7af2846eb39b61e203ca2e8ee158a29b1
Will Jones <the8bitsquid@gmail.com>
2022-07-17 01:37:02 -0500

    half-done dice ray casting

--- Scenes/Mountain/Dice/Die.gd
+++ Scenes/Mountain/Dice/Die.gd
@@ -1,6 +1,6 @@
 extends RigidBody
 
-signal monster_hit(monster_ref)
+signal monster_hit
 signal dice_switched
 
 export var rolling_force = 10
@@ -22,7 +22,10 @@ func _process(delta):
 		flash_counter += flash_speed
 		var c = flash_amount * abs(sin(flash_counter * delta))
 		set_highlight(c)
-
+	
+	for child in get_child(0).get_children():
+		if child is RayCast and child.is_colliding():
+			print(child.name)
 
 func _physics_process(delta):
 	
@@ -41,6 +44,10 @@ func _physics_process(delta):
 		angular_velocity.x = clamp(angular_velocity.x - (rolling_force*delta), -1 * max_angular_velocity, max_angular_velocity)
 	elif Input.is_action_pressed("right"):
 		angular_velocity.x = clamp(angular_velocity.x + (rolling_force*delta), -1 * max_angular_velocity, max_angular_velocity)
+		
+func _input(event):
+	if Input.is_action_pressed("ui_accept"):
+		switch_dice()
 
 func start_flash():
 	invincible = true
